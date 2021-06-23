9317bbaee528a41e375199836fdfd5eb84732667
ELGEOR-PC\elgeor <elgeor.sk@gmail.com>
2021-06-23 15:48:20 +0200

    test: Add getPlace call

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -13,11 +13,14 @@ public class Basics {
         RestAssured.baseURI = "https://rahulshettyacademy.com";
 
         //addPlace - POST
-        String responseAddPlace = given().queryParam("key",key).header("Content-Type", "application/json")
+        String responseAddPlace = given().queryParam("key",key)
+                .header("Content-Type", "application/json")
                 .body(Payload.addPlace())
                 .when().post("maps/api/place/add/json")
-                .then().assertThat().statusCode(200).body("scope", equalTo("APP"))
-                .header("Server","Apache/2.4.18 (Ubuntu)").extract().response().asString();
+                .then().assertThat().statusCode(200)
+                .body("scope", equalTo("APP"))
+                .header("Server","Apache/2.4.18 (Ubuntu)")
+                .extract().response().asString();
 
         System.out.println(responseAddPlace);
 
@@ -28,12 +31,25 @@ public class Basics {
         System.out.println(placeId);
 
         //updatePlace - PUT
-        String responseUpdatePlace = given().queryParam("key",key).header("Content-Type", "application/json")
+        String responseUpdatePlace = given().queryParam("key",key)
+                .header("Content-Type", "application/json")
                 .body(Payload.updatePlace(key,placeId))
                 .when().put("maps/api/place/update/json")
-                .then().assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"))
+                .then().assertThat().statusCode(200)
+                .body("msg", equalTo("Address successfully updated"))
                 .extract().response().asString();
 
         System.out.println(responseUpdatePlace);
+
+        //getPlace - GET
+        String responseGetPlace = given().queryParam("key",key)
+                .queryParam("place_id",placeId)
+                .header("Content-Type", "application/json")
+                .when().get("maps/api/place/get/json")
+                .then().assertThat().statusCode(200)
+                .body("address", equalTo("70 winter walk, USA"))
+                .extract().response().asString();
+
+        System.out.println(responseGetPlace);
     }
 }
