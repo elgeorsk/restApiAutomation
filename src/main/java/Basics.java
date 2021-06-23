53dbfeaca44044edbd4812cdda14e679740d651d
ELGEOR-PC\elgeor <elgeor.sk@gmail.com>
2021-06-23 14:42:26 +0200

    test: Add addPlace and updatePlace calls
    Initial submit
    Use RestAssured to represened addPlace(POST) and updatePlace(PUT) calls

---
+++ src/main/java/Basics.java
@@ -0,0 +1,39 @@
+import files.Payload;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Basics {
+    public static void main(String[] args){
+
+        String key = "qaclick123";
+        // validate addPlace API is working as expected
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+
+        //addPlace - POST
+        String responseAddPlace = given().queryParam("key",key).header("Content-Type", "application/json")
+                .body(Payload.addPlace())
+                .when().post("maps/api/place/add/json")
+                .then().assertThat().statusCode(200).body("scope", equalTo("APP"))
+                .header("Server","Apache/2.4.18 (Ubuntu)").extract().response().asString();
+
+        System.out.println(responseAddPlace);
+
+        // to parse json
+        JsonPath json = new JsonPath(responseAddPlace);
+
+        String placeId = json.getString("place_id");
+        System.out.println(placeId);
+
+        //updatePlace - PUT
+        String responseUpdatePlace = given().queryParam("key",key).header("Content-Type", "application/json")
+                .body(Payload.updatePlace(key,placeId))
+                .when().put("maps/api/place/update/json")
+                .then().assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"))
+                .extract().response().asString();
+
+        System.out.println(responseUpdatePlace);
+    }
+}
